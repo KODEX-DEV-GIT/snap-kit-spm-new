83cd10a6e85304fa7f09de5969e79cbea575c5fd
Kodex <205018495+KODEX-DEV-GIT@users.noreply.github.com>
2026-02-23 12:12:15 +0500

    Convert binary targets to url+checksum for remote SPM support

--- Package.swift
+++ Package.swift
@@ -17,19 +17,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SCSDKCoreKit",
-            path: "SCSDKCoreKit.xcframework"
+            url: "https://raw.githubusercontent.com/KODEX-DEV-GIT/snap-kit-spm-new/main/SCSDKCoreKit.xcframework.zip",
+            checksum: "55dfe75edbcf1170fbfac1639b54ae2be74c7c23946bbefd54b7e81365f970a1"
         ),
         .binaryTarget(
             name: "SCSDKBitmojiKit",
-            path: "SCSDKBitmojiKit.xcframework"
+            url: "https://raw.githubusercontent.com/KODEX-DEV-GIT/snap-kit-spm-new/main/SCSDKBitmojiKit.xcframework.zip",
+            checksum: "34f9bfa027c1cd0a6f44b6e424112b72f87a54fd841ef7eb9082eb96fba4409e"
         ),
         .binaryTarget(
             name: "SCSDKLoginKit",
-            path: "SCSDKLoginKit.xcframework"
+            url: "https://raw.githubusercontent.com/KODEX-DEV-GIT/snap-kit-spm-new/main/SCSDKLoginKit.xcframework.zip",
+            checksum: "9efe82ee275ba7ec4593fb254f999eca930eb255b7eeea2844d6c623d77a0f5c"
         ),
         .binaryTarget(
             name: "SCSDKCreativeKit",
-            path: "SCSDKCreativeKit.xcframework"
+            url: "https://raw.githubusercontent.com/KODEX-DEV-GIT/snap-kit-spm-new/main/SCSDKCreativeKit.xcframework.zip",
+            checksum: "a1ad9532811cd2b38fec941bcae30fca89133d4ca461ec6b402a0ebed4cd48de"
         ),
     ]
 )
